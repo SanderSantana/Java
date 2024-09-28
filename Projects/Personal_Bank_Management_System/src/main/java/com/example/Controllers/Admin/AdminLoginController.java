9bc010b0905c3334c2d6d56f6cb9851335f4e29e
Sander Santana <piratesnafer30san@gmail.com>
2024-09-28 23:21:56 +0200

    Finished the database and now working on implementing the queries.

--- Projects/Personal_Bank_Management_System/src/main/java/com/example/Controllers/Admin/AdminLoginController.java
+++ Projects/Personal_Bank_Management_System/src/main/java/com/example/Controllers/Admin/AdminLoginController.java
@@ -1,14 +1,21 @@
 package com.example.Controllers.Admin;
 
+import com.example.DatabaseConnection;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class AdminLoginController {
 
@@ -16,6 +23,10 @@ public class AdminLoginController {
     private Scene scene;
     @FXML
     private Stage stage;
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private PasswordField passwordField;
 
     public void AdminLoginPage(ActionEvent e) throws IOException {
 
@@ -29,8 +40,50 @@ public class AdminLoginController {
 
     public void loginButtonOnAction(ActionEvent e) throws IOException {
 
+        validateLogin(e);
+
+
+    }
+    public void validateLogin(ActionEvent e){
+
+        DatabaseConnection connection = new DatabaseConnection();
+
+        Connection connectionDb = connection.getConnection();
+
         CreateUsersController createUsersController = new CreateUsersController();
-        createUsersController.CreateUsersPage(e);
+
+
+        String verifyLogin = "SELECT COUNT(1) FROM Admin WHERE Username = '" + usernameField.getText() + "' AND Password = '" + passwordField.getText() + "'";
+
+        try {
+
+            Statement statement = connectionDb.createStatement(); //we need a statement object to execute the query.
+            ResultSet queryResult = statement.executeQuery(verifyLogin); //we need a ResultSet object to hold the result of our query.
+
+            //The while statement is going to iterate over all rows
+            while(queryResult.next())  {
+
+                if (queryResult.getInt(1) == 1) { //checks if the result in the first column is 1
+
+                    createUsersController.CreateUsersPage(e);
+
+                }
+                else {
+
+//                    loginWarningLabel.setText("Invalid Login");
+//                    loginWarningLabel.setStyle("-fx-text-fill: red;" + "-fx-font-weight: bold;" + "-fx-font-size: 14");
+
+                }
+
+            }
+
+        }
+
+        catch (Exception a) {
+
+            a.printStackTrace();
+
+        }
 
     }
 
--- Projects/Personal_Bank_Management_System/src/main/java/com/example/DatabaseConnection.java
+++ Projects/Personal_Bank_Management_System/src/main/java/com/example/DatabaseConnection.java
@@ -1,4 +1,32 @@
 package com.example;
 
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+//This class establishes the connection with the database
 public class DatabaseConnection {
+
+    public Connection databaseLink;
+
+    //This method is used to connect to the database whenever is necessary
+    public Connection getConnection() {
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            databaseLink = DriverManager.getConnection("jdbc:mysql://localhost:3306/PersonalBakingSystem", "root", "Ss32566565");
+
+        }
+        catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
+
+        return databaseLink;
+
+    }
+
 }
